4c7c5c20708165d70fb1625b5ee3cd45da1ea0cf
Стив <kenethra.74@gmail.com>
2023-05-06 11:01:23 -0600

    feat: implement visits directory

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -1,6 +1,12 @@
 package project
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+
 	"github.com/karchx/nrs/pkg/todo"
 )
 
@@ -25,6 +31,27 @@ type Project struct {
 
 // WalkTodosOfDir visits all of the TODOs in a particular directory.
 func (project Project) WalkTodosOfDir(dirpath string, visit func(todo.Todo) error) error {
-  //cmd := exec.Command("git", "ls-files", dirpath)
+  cmd := exec.Command("git", "ls-files", dirpath)
+  var outBuffer bytes.Buffer
+  cmd.Stdout = &outBuffer
+
+  err := cmd.Run()
+  if err != nil {
+    return err
+  }
+
+  for scanner := bufio.NewScanner(&outBuffer); scanner.Scan(); {
+    filepath := scanner.Text()
+    stat, err := os.Stat(filepath)
+    if err != nil {
+      return err
+    }
+  
+    if stat.IsDir() {
+      fmt.Printf("[WARN] `%s` is probably a submodule. Skipping it for now...\n", filepath)
+      continue
+    }
+  }
+
   return nil
 }
